e06807631b011074595f58a4b73bbe9d257ee9d4
Angie Mnq <angiemanquillo5@gmail.com>
2026-05-01 18:46:58 -0500

    Modify ciclo-while.js to change attempt logic

--- ciclo-while.js
+++ ciclo-while.js
@@ -1,12 +1,12 @@
-let intento = 1
-const maximoIntentos = 3
+let intento = 5
+const maximoIntentos = 200
 
-console.log("Práctica inicial con while")
+console.log("Práctica inicial con el comando while")
 console.log("Vamos a repetir un proceso mientras el intento sea menor o igual al máximo.")
 
 while (intento <= maximoIntentos) {
   console.log("Intento de revisión número:", intento)
-  intento = intento + 1
+  intento = intento + 5
 }
 
-console.log("Proceso de revisión finalizado.")
+console.log("Proceso de revisión terminado.")
