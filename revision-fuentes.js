f3244da4e76869a6abf7b20a44251247e8a4a966
Angie Mnq <angiemanquillo5@gmail.com>
2026-05-01 18:40:24 -0500

    Update fuenteActual and totalFuentes values

--- revision-fuentes.js
+++ revision-fuentes.js
@@ -1,11 +1,11 @@
-let fuenteActual = 1
-const totalFuentes = 4
+let fuenteActual = 2
+const totalFuentes = 9
 
-console.log("Revisión de fuentes antes de compartir información")
+console.log("Revisión de fuentes antes de enviar información")
 
 while (fuenteActual <= totalFuentes) {
   console.log("Revisando fuente número:", fuenteActual)
   fuenteActual = fuenteActual + 1
 }
 
-console.log("Proceso de revisión de fuentes terminado.")
+console.log("Proceso de revisión de fuentes finalizado.")
