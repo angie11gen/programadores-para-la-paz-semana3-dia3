042a02cb2f9466cde2a2d34afffc4b697a522962
Angie Mnq <angiemanquillo5@gmail.com>
2026-05-01 18:44:34 -0500

    Update proposal counting logic and messages

--- conteo-propuestas.js
+++ conteo-propuestas.js
@@ -1,11 +1,11 @@
-let propuestaActual = 1
-const totalPropuestas = 5
+let propuestaActual = 3
+const totalPropuestas = 60
 
-console.log("Conteo de propuestas comunitarias")
+console.log("Conteo de respuestas para propuestas comunitarias")
 
 while (propuestaActual <= totalPropuestas) {
   console.log("Revisando propuesta número:", propuestaActual)
-  propuestaActual = propuestaActual + 1
+  propuestaActual = propuestaActual + 3
 }
 
-console.log("Todas las propuestas fueron revisadas.")
+console.log("Todas las propuestas fueron analizadas.")
